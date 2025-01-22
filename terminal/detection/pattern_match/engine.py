e3590355dd2f300614531569c370efcfe39308e3
Sampriti2803 <sampritisaha@gmail.com>
2025-01-22 23:58:51 +0530

    changed engine.py to test

--- terminal/detection/pattern_match/engine.py
+++ terminal/detection/pattern_match/engine.py
@@ -1,6 +1,6 @@
 import json
-from .matcher import PatternMatcher
-from .heuristic import HeuristicAnalyzer
+from matcher import PatternMatcher
+from heuristic import HeuristicAnalyzer
 
 class DetectionEngine:
     def __init__(self, rules_path):
@@ -19,11 +19,12 @@ class DetectionEngine:
 
 if __name__ == "__main__":
     # Example usage
-    rules_path = "pattern_match/rules/"
+    rules_path = "terminal/detection/pattern_match/rules"
     engine = DetectionEngine(rules_path)
-    sample_data = {
-        "domain": "malicious-site.ru",
-        "content": "Some suspicious traffic details.",
-    }
+    # sample_data = {
+    #     "domain": "malicious-site.ru",
+    #     "content": "Some suspicious traffic details.",
+    # }
+    sample_data={'src_ip': None, 'dst_ip': None, 'src_port': 57082, 'dst_port': 3478, 'payload_size': 1210}
     result = engine.detect(sample_data)
     print(json.dumps(result, indent=4))
